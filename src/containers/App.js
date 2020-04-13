364004f36d0b598e7a1e94c4f282041268881603
mwstarbuck <mwstarbuck@gmail.com>
2020-04-13 09:44:10 -0500

    splitting app into components

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,8 +7,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import './App.css';
-
-import Person from './Person/Person';
+import Persons from '../components/Persons/Persons';
 
 const StyledButton = styled.button`
 
@@ -111,17 +110,11 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangedHandler(event, person.id)} />
-          })}
-
+          <Persons
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler} />
         </div>
-
       );
 
       style.backgroundColor = 'red';
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root')); // Gets access to the root element in index.html in the public folder
